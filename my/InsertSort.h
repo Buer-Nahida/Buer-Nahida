363864a7bd2906dad087ffebcba09b1d9c9c50f5
star <yanwenz551@gmail.com>
2023-08-26 02:55:33 +0800

    尝试修复插入排序，但没成功 	修改：     my/InsertSort.h 	修改：     test.cpp

--- my/InsertSort.h
+++ my/InsertSort.h
@@ -2,27 +2,28 @@
 #include <iostream>
 using namespace std;
 
-template <typename T> void InsertSort(T &arrName, bool ascendingSort) {
-  unsigned long long count1, arrLenger = (sizeof arrName / sizeof arrName[0]);
+template <typename T, unsigned M>
+void InsertSort(T (&arrName)[M], bool ascendingSort) {
+  unsigned long long count1, arrLenger = (sizeof arrName / sizeof *arrName);
   T arrTemp;
   for (int count0 = 0; count0 < arrLenger; count0++) {
-    arrTemp[0] = arrName[count0];
+    arrTemp = arrName[count0];
     count1 = count0 - 1;
     while (count1 >= 0) {
       if (ascendingSort) {
-        if (arrTemp[0] <= arrName[count1]) {
+        if (arrTemp <= arrName[count1]) {
           arrName[count1 + 1] = arrName[count1];
           count1 -= 1;
         } else
           break;
       } else {
-        if (arrTemp[0] >= arrName[count1]) {
+        if (arrTemp >= arrName[count1]) {
           arrName[count1 + 1] = arrName[count1];
           count1 -= 1;
         } else
           break;
       }
     }
-    arrName[count1 + 1] = arrTemp[0];
+    arrName[count1 + 1] = arrTemp;
   }
 }
--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
 #include "my/star.h"
 
+#ifdef Lenger
+template <typename T, unsigned M> int len(T (&t)[M]) {
+  return sizeof(t) / sizeof(*t);
+}
+#endif
+
 int main(int argc, char *argv[]) {
-  double b[] = {1.1, 1.9, 4.1, 5.9, 1.8, 4.1};
-  PrintArray(b);
-  BubbleSort(b, 0);
-  PrintArray(b);
   double a[] = {1.1, 1.9, 4.1, 5.9, 1.8, 4.1};
   PrintArray(a);
   SelectSort(a, 0);
   PrintArray(a);
+  double b[] = {1.1, 1.9, 4.1, 5.9, 1.8, 4.1};
+  PrintArray(b);
+  BubbleSort(b, 0);
+  PrintArray(b);
   return 0;
 }
