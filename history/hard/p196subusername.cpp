e8c69153e544c617e74c22b5784392933e3b86d4
star <yanwenz551@gmail.com>
2023-08-28 02:24:51 +0800

    修复P196小案例头文件和用户名里面有@无法正常截取的bug 	修改：     p196subusername.cpp

--- history/hard/p196subusername.cpp
+++ history/hard/p196subusername.cpp
@@ -1,7 +1,9 @@
-#include "my/star.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, char *argv[]) {
   string strEmail = "myemail@gmail.com";
-  cout << strEmail.substr(0, strEmail.find('@')) << endl;
+  cout << strEmail.substr(0, strEmail.rfind('@')) << endl;
   return 0;
 }
