b5e983e73cc809d9270a7f5f4fcebe23f9322c72
star <yanwenz551@gmail.com>
2023-08-27 23:51:00 +0800

    去掉PrintArray.hpp中for循环的大括号 	修改：     my/PrintArray.hpp 	修改：     test.cpp

--- my/PrintArray.hpp
+++ my/PrintArray.hpp
@@ -3,10 +3,9 @@ using namespace std;
 
 template <typename T> void PrintArray(T &arrName) {
   unsigned long long arrLenger = (sizeof arrName / sizeof arrName[0]);
-  for (unsigned long long count = 0; count < arrLenger; count++) {
+  for (unsigned long long count = 0; count < arrLenger; count++)
     if (count == arrLenger - 1)
       cout << arrName[count] << endl;
     else
       cout << arrName[count] << " ";
-  }
 }
--- test.cpp
+++ test.cpp
@@ -1,6 +1,47 @@
 #include "my/star.h"
 
+class Test {
+public:
+  Test(string name, int age) {
+    m_Name = name;
+    m_Age = age;
+  }
+  Test(Test &&) = default;
+  Test(const Test &) = default;
+  Test &operator=(Test &&) = default;
+  Test &operator=(const Test &) = default;
+  ~Test() = default;
+  string m_Name;
+  int m_Age;
+};
+
+ostream &operator<<(ostream &outPut, Test &testClass) {
+  outPut << testClass.m_Age << " " << testClass.m_Name;
+  return outPut;
+}
+
 int main(int argc, char *argv[]) {
-  cout << "Hello world!\n";
+  string str;
+
+  char a[] = "Hello world!";
+  string strCharArr;
+  /* strCharArr = a; */
+
+  /* strCharArr.assign("Hello Cpp"); */
+  /* strCharArr.assign(a); */
+
+  /* strCharArr.assign(a, 4); */
+  strCharArr.assign("Hello Cpp", 5);
+  cout << strCharArr << endl;
+
+  string strStr;
+  /* strStr = strCharArr; */
+  strStr.assign(strCharArr);
+  cout << strStr << endl;
+
+  string strChar;
+  /* strChar = 'a'; */
+  strChar.assign(2, 'a');
+  cout << strChar << endl;
   return 0;
 }
