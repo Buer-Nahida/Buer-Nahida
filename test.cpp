9ae80543f879ad265e9ac5d38833f3c5bafc8e77
star <yanwenz551@gmail.com>
2023-10-06 21:22:10 +0800

    fix(speechsystem): 修复演讲比赛管理系统保存文件格式错误的bug

--- test.cpp
+++ test.cpp
@@ -1,18 +1,13 @@
 #include "my/star.h"
-#include <algorithm>
-#include <array>
+#include <fstream>
+#include <ios>
 #include <iostream>
 using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 
 int main(int argc, char *argv[]) {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    array<int, 3> j{4, 5, 1};
-    array<int, 6> i{5, 1, 3, 6, 3, 8};
-    array<int, 9> x{};
-    merge(i.begin(), i.end(), j.begin(), j.end(), x.begin());
-    star::PrintAll(x);
-    MergeSort<int, 9>(x);
-    star::PrintAll(x);
+    int *nullp = NULL;
+    ifstream fin;
     return 0;
 }
