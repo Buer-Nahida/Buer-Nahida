06f687ce5a664f063e611a8df816c6015c80b3a3
star <yanwenz551@gmail.com>
2023-10-27 23:30:11 +0800

    feat(submodule): Remove my/ directory, link git@github.com:SR-MyStar/Star-Standard-Library.git repo to my/ directory 	新文件：   .gitmodules 	新文件：   my 	删除：     my/.clang-format 	删除：     my/.gitignore 	删除：     my/LICENSE 	删除：     my/README.md 	删除：     my/algorithm/other/merge.hpp 	删除：     my/algorithm/other/print_all.hpp 	删除：     my/algorithm/sort/bubble_sort.hpp 	删除：     my/algorithm/sort/heap_sort.hpp 	删除：     my/algorithm/sort/insert_sort.hpp 	删除：     my/algorithm/sort/merge_sort.hpp 	删除：     my/algorithm/sort/select_sort.hpp 	删除：     my/containers/list_node.hpp 	删除：     my/star.h 	修改：     test.cpp

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "my/star.h"
+#include <iostream>
 #include <sys/sendfile.h>
 #include <vector>
 #define true  0
@@ -6,5 +7,9 @@
 
 int main(int argc, char *argv[]) {
     std::vector<long long> a{1, 2, 6, 4, 3, 5, 7, 5, 9, 4};
+    int                    ans = 0;
+    std::cout << ans << '\n';
+    ans ^= (~ans & -~ans) | ((~ans & -~ans) + ans + ~ans);
+    std::cout << ans << '\n';
     return 0;
 }
